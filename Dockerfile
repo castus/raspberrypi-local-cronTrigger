196afbd972a3bf61cc740e462134db9c814b7466
Krzysztof Romanowski <krzysztof.romanowski@zendesk.com>
2022-12-18 22:50:36 +0100

    Add Docker compose support

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,13 @@
 FROM golang:latest
 
+WORKDIR /data
+
 RUN apt-get update
 RUN apt-get upgrade -y
 RUN apt-get install nano -y
 
+COPY ./src /data
+
 ENV LANG en_US.utf8
+
+CMD sh run.sh
--- docker-run.sh
+++ docker-run.sh
@@ -5,9 +5,10 @@ docker run \
   --name raspberrypiLocal-cronTrigger \
   -v "$(pwd)"/src:/data \
   --workdir /data \
-  --env-file=.env.prod \
+  --env-file=.env \
+  --env-file=.env.dev \
   --env-file=.env.secrets \
   --net mqtt-network \
   -itd \
-  raspberrypiLocal-cronTrigger-img \
+  c4stus/raspberrypi:crontrigger \
   /bin/bash -c "sh run.sh"
